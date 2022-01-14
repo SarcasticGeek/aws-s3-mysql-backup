33e3fcbbae78e1ed548438d96c3a44599553f487
Mohamed Essam Fathalla <mohamedessamfathalla@gmail.com>
2022-01-14 23:32:13 +0200

    wip

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql');
 var { exec } = require("child_process");
+var fs = require('fs');
+var AWS = require('aws-sdk');
+
 require('dotenv').config()
 
 var con = mysql.createConnection({
@@ -8,15 +11,31 @@ var con = mysql.createConnection({
   password: process.env.DB_PASS,
 });
 
+var s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
 con.connect(function(err) {
-  if (err) throw err;
+  if (err) console.log(err);
   con.query("show databases", function (err, result, fields) {
-    if (err) throw err;
-    let today = new Date().toISOString();
+    if (err) console.log(err);
+    let today = new Date().toISOString().substring(0, 10);
     result.forEach(database => {
-      exec(`mysqldump ${database.Database} -u ${process.env.DB_USER} --password=${process.env.DB_PASS} | gzip > ./files/${today}${database.Database}.sql.gz`)
-      exec(`aws s3 cp ./files/${today}${database.Database}.sql.gz s3://${process.env.S3_REPO}`)
-      exec(`git rm ./files/${today}${database.Database}sql.gz`)
+      exec(`mysqldump ${database.Database} -u ${process.env.DB_USER} --password=${process.env.DB_PASS} | gzip > ./files/${today}-${database.Database}.sql.gz`)
+      fs.readFile(`./files/${today}-${database.Database}.sql.gz`, (err, data) => {
+        if (err) console.log(err);
+        let params = {
+          Bucket: process.env.S3_REPO,
+          Key: `${today}-${database.Database}.sql.gz`,
+          Body: data
+          }
+        s3.upload(params, function(s3Err, data) {
+            if (s3Err) console.log(s3Err);
+            console.log(`File uploaded successfully at ${data.Location}`)
+            exec(`rm ./files/${today}-${database.Database}.sql.gz`)
+        });
+       });
     });
     process.exit();
   });
