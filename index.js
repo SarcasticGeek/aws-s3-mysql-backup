a18bd19b316f273eee710f106153967e2ecd044d
Mohamed Essam Fathalla <mohamedessamfathalla@gmail.com>
2022-01-14 23:01:17 +0200

    fixes in execs

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ con.connect(function(err) {
     if (err) throw err;
     let today = new Date().toISOString();
     result.forEach(database => {
-      exec(`mysqldump ${database.Database} -u ${process.env.DB_USER} --password=${process.env.DB_PASS} | gzip > ./files/${today}${database.Database}.sql.gz
-        && aws s3 cp ./files/${today}${database.Database}.sql.gz s3://${process.env.S3_REPO} 
-        && git rm ./files/${today}${database.Database}sql.gz
-      `)
+      exec(`mysqldump ${database.Database} -u ${process.env.DB_USER} --password=${process.env.DB_PASS} | gzip > ./files/${today}${database.Database}.sql.gz`)
+      exec(`aws s3 cp ./files/${today}${database.Database}.sql.gz s3://${process.env.S3_REPO}`)
+      exec(`git rm ./files/${today}${database.Database}sql.gz`)
     });
     process.exit();
   });
