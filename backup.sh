875be9267110b505ca58f91836c13144d1da1ef6
Mohamed Essam Fathalla <mohamedessamfathalla@gmail.com>
2022-01-22 03:18:27 +0200

    add --column-statistics=0

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ IFS=',' read -r -a databases <<< "$DATABASES"
 
 for database in "${databases[@]}"; do
     echo "backing: $database"
-    mysqldump "$database" -h "$DB_HOST" -u "$DB_USER" --password="$DB_PASS" | gzip > `date +"%Y-%m-%d"-$database.sql.gz` && \
+    mysqldump --column-statistics=0 "$database" -h "$DB_HOST" -u "$DB_USER" --password="$DB_PASS" | gzip > `date +"%Y-%m-%d"-$database.sql.gz` && \
     aws s3 cp `date +"%Y-%m-%d"-$database.sql.gz` "s3://$S3_REPO" && \
     rm `date +"%Y-%m-%d"-$database.sql.gz`
 done
