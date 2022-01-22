ad0010f13294e59094a91579cd43e1832bff0e9d
Mohamed Essam Fathalla <mohamedessamfathalla@gmail.com>
2022-01-22 03:13:28 +0200

    fixes

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ IFS=',' read -r -a databases <<< "$DATABASES"
 for database in "${databases[@]}"; do
     echo "backing: $database"
     mysqldump "$database" -h "$DB_HOST" -u "$DB_USER" --password="$DB_PASS" | gzip > `date +"%Y-%m-%d"-$database.sql.gz` && \
-    aws s3 cp `date +"%Y-%m-%d"-$database.sql.gz` `s3://$S3_REPO` && \
+    aws s3 cp `date +"%Y-%m-%d"-$database.sql.gz` "s3://$S3_REPO" && \
     rm `date +"%Y-%m-%d"-$database.sql.gz`
 done
 
